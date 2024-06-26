fa572a88ea4eff242ca6f23082fa0e461fa26fb3
Fineas <r0845664@student.thomasmore.be>
2024-06-26 17:18:30 +0200

    created frontend folder, react app, created folders for Card and Pages components and made Card Component

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Card.css"
+//type Props = {};
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="detail">
+        <h2>Test</h2>
+        <p>$</p>
+      </div>
+      <p className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, enim!</p>
+    </div>
+  );
+};
+
+export default Card;
