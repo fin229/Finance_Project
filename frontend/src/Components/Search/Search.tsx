eb5f9b4dbec1ea767e8539e3ea94cd81059a99be
Fineas <r0845664@student.thomasmore.be>
2024-06-28 10:41:28 +0200

    added API

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { ChangeEvent, FC, useState, SyntheticEvent } from "react";
 import "./Search.css"
 
 interface Props {};
@@ -6,13 +6,19 @@ interface Props {};
 const Search:FC<Props> = () => {
     const [search,setSearch]=useState<string>("");
 
-    const onclick=(e:any)=>{
+    const handleChange=(e:ChangeEvent<HTMLInputElement>)=>{
         setSearch(e.target.value);
         console.log(e);
     }
+    
+    const onClick=(e:SyntheticEvent)=>{
+      console.log(e)
+    };
+
   return (
    <div>
-    <input value={search} onChange={(e)=>onclick(e)}></input>
+    <input value={search} onChange={(e)=>handleChange(e)}></input>
+    <button onClick={(e)=>onClick(e)}></button>
    </div>
   );
 };
